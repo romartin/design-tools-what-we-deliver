13478fe4927ab7ac594c212c1b043c3cdadb465f
Roger Martinez <roger600@gmail.com>
2020-12-03 22:13:20 +0100

    scripts

---
+++ clone-repos.sh
@@ -0,0 +1,17 @@
+git clone https://github.com/kiegroup/appformer.git
+git clone https://github.com/kiegroup/drools.git
+git clone https://github.com/kiegroup/drools-wb.git
+git clone https://github.com/kiegroup/droolsjbpm-build-bootstrap.git
+git clone https://github.com/kiegroup/droolsjbpm-integration.git
+git clone https://github.com/kiegroup/droolsjbpm-knowledge.git
+https://github.com/errai/errai.git
+git clone https://github.com/kiegroup/gwt-jsonix-schema-compiler.git
+git clone https://github.com/kiegroup/jbpm-wb.git
+git clone https://github.com/kiegroup/kie-docs.git
+git clone https://github.com/kiegroup/kie-soup.git
+git clone https://github.com/kiegroup/kie-wb-common.git
+git clone https://github.com/kiegroup/kogito-runtimes.git
+git clone https://github.com/kiegroup/kogito-tooling.git
+git clone https://github.com/kiegroup/lienzo-core.git
+git clone https://github.com/kiegroup/lienzo-tests.git
+
---
+++ what-did-we-deliver.sh
@@ -0,0 +1,32 @@
+
+what-did-we-deliver-here() {
+
+  # SHHH! zsh 
+  setopt localoptions rmstarsilent
+
+  echo "=> $1"
+  cd "/Users/karreiro/Projects/forks/$1"
+  git checkout what-did-we-deliver > /dev/null 2>&1
+  git fetch upstream > /dev/null 2>&1
+  git reset upstream/master --hard > /dev/null 2>&1
+  git log --pretty=format:"%an (%ar): %s" --after="$2" --until="$3" --author="David|Alex|Myriam|Guilherme Car|Daniel |Handrey|Jaime|Kirill|Roger|Toni|Valentino|Wagner|Yeser|Dominik|Jan|Jozef|Lubomir|Liz|Heena|Stetson"
+}
+
+what-did-we-deliver() {
+  what-did-we-deliver-here "appformer" "$1" "$2"
+  what-did-we-deliver-here "drools" "$1" "$2"
+  what-did-we-deliver-here "drools-wb" "$1" "$2"
+  what-did-we-deliver-here "droolsjbpm-build-bootstrap" "$1" "$2"
+  what-did-we-deliver-here "droolsjbpm-integration" "$1" "$2"
+  what-did-we-deliver-here "droolsjbpm-knowledge" "$1" "$2"
+  what-did-we-deliver-here "errai" "$1" "$2"
+  what-did-we-deliver-here "gwt-jsonix-schema-compiler" "$1" "$2"
+  what-did-we-deliver-here "jbpm-wb" "$1" "$2"
+  what-did-we-deliver-here "kie-docs" "$1" "$2"
+  what-did-we-deliver-here "kie-soup" "$1" "$2"
+  what-did-we-deliver-here "kie-wb-common" "$1" "$2"
+  what-did-we-deliver-here "kogito-runtimes" "$1" "$2"
+  what-did-we-deliver-here "kogito-tooling" "$1" "$2"
+  what-did-we-deliver-here "lienzo-core" "$1" "$2"
+  what-did-we-deliver-here "lienzo-tests" "$1" "$2"
+}
